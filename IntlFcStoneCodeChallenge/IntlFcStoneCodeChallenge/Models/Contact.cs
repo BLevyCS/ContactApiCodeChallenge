55ff9f50b0f721ec9613ae13e7f995b7651d0a69
Brian Levy <brian@levysplace.us>
2019-09-13 17:03:50 -0500

    Adds model for contact

---
+++ IntlFcStoneCodeChallenge/IntlFcStoneCodeChallenge/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntlFcStoneCodeChallenge.Models
+{
+    public class Contact
+    {
+        public long Id { get; }
+        public string Name { get; private set; }
+        public string Company { get; private set; }
+        public string ProfileImageFilename { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public int WorkPhone { get; private set; }
+        public int PersonalPhone { get; private set; }
+        public string Address { get; private set; }
+    }
+}
