9fd311b84d7fe9d213b18eca6cc677561a05925b
BLevyCS <Brian@levysplace.us>
2019-09-15 13:51:27 -0500

    Deletes DbContext to go with a different method for transporting data

--- IntlFcStoneCodeChallenge/IntlFcStoneCodeChallenge/Controllers/ContactController.cs
+++ IntlFcStoneCodeChallenge/IntlFcStoneCodeChallenge/Controllers/ContactController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web.Http;
-using IntlFcStoneCodeChallenge.Context;
 using IntlFcStoneCodeChallenge.Models;
 using Microsoft.AspNetCore.Mvc;
 using FromBodyAttribute = Microsoft.AspNetCore.Mvc.FromBodyAttribute;
@@ -17,21 +16,11 @@ namespace IntlFcStoneCodeChallenge.Controllers
     public class ContactController : ControllerBase
     {
 
-        private readonly ApplicationDbContext _context;
-
 
         // POST api/contact/[contact]
         [HttpPost]
         public Contact Create([FromBody] Contact contact)
         {
-            if (!ModelState.IsValid)
-            {
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
-            }
-
-            _context.Contacts.Add(contact);
-            _context.SaveChanges();
-
             return contact;
         }
     }
