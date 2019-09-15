a93e9d214dfa26431c75e5b8a2fe92732842c5fe
BLevyCS <Brian@levysplace.us>
2019-09-15 13:13:48 -0500

    Adds POST method and DbContext to ContactController

--- IntlFcStoneCodeChallenge/IntlFcStoneCodeChallenge/Controllers/ContactController.cs
+++ IntlFcStoneCodeChallenge/IntlFcStoneCodeChallenge/Controllers/ContactController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IntlFcStoneCodeChallenge.Context;
+using IntlFcStoneCodeChallenge.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IntlFcStoneCodeChallenge.Controllers
@@ -10,11 +12,23 @@ namespace IntlFcStoneCodeChallenge.Controllers
     [ApiController]
     public class ContactController : ControllerBase
     {
+
+        private readonly ApplicationDbContext _context;
+
+
         // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        [HttpPost]
+        public ActionResult<IEnumerable<string>> Create([FromBody] Contact contact)
         {
-            return new string[] { "value1", "value2" };
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+
+            return contact;
         }
     }
 }
