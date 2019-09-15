7e11e94b3ab9f695b868e24dfc1f731fa4d73bec
BLevyCS <Brian@levysplace.us>
2019-09-15 16:05:20 -0500

    Removes unneeded usings

--- IntlFcStoneCodeChallenge/IntlFcStoneCodeChallenge/Data/ContactRepository.cs
+++ IntlFcStoneCodeChallenge/IntlFcStoneCodeChallenge/Data/ContactRepository.cs
@@ -1,9 +1,6 @@
 ﻿using IntlFcStoneCodeChallenge.Interfaces;
 using IntlFcStoneCodeChallenge.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace IntlFcStoneCodeChallenge.Data
 {
--- IntlFcStoneCodeChallenge/IntlFcStoneCodeChallenge/Interfaces/IContactRepository.cs
+++ IntlFcStoneCodeChallenge/IntlFcStoneCodeChallenge/Interfaces/IContactRepository.cs
@@ -1,8 +1,4 @@
 ﻿using IntlFcStoneCodeChallenge.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace IntlFcStoneCodeChallenge.Interfaces
 {
